220292ddef2e2805fb0df8c34e1c1f0da2a7a25e
Sumit Pal Singh <67005081+sumit-repository@users.noreply.github.com>
2024-02-05 00:02:20 +0530

    Update custom_string.cpp

--- custom_string.cpp
+++ custom_string.cpp
@@ -2,50 +2,83 @@
 //String class implementation
 //to be continued from: https://www.youtube.com/watch?v=uQB7jNWnZaI
 
-// Online C++ compiler to run C++ program online
-#include <iostream>
+//#include "String.h"
+//#pragma once
+#include<iostream>
 using namespace std;
 
-class String {
-    private:
-        //const int i;
-        //char* res;
-        unsigned int len;
-        void strcpy(const char*ch);
-    public:
-        char* res;
-        String():res(nullptr), len(0){}
-        String(const char* ch) {
-            //strlen method needs to be used with #include<cstring>
-            //len = strlen(ch); 
-            len=0;
-            while(ch[len]!='\0') 
-                ++len;
-            res = new char[len+1];
-            //cout << ch << endl;
-            strcpy(ch);
-        }
+class String
+{
+private:
+	unsigned int len;
+	char* str;
+	void strcpy(const char* ch);
+public:
+	String() :str(nullptr), len(0) {}
+	String(const char* ch);
+	String(const String & obj);
+	const int length() { return len; }
+	String& operator= (const String& obj); //Refer Note 1
 };
 
-void String::strcpy(const char* ch) {
-    if (ch == nullptr)
-        return;
-    char* temp = res;
-    while (*ch != '\0') {
-        *temp++ = *ch++;
-    }
-    *temp = '\0';
-    //another way while((*temp++=*ch++)!='0'){}
-}
-
-std::ostream& operator<<(std::ostream& stream, String s) {
-    stream << s.res ;
-    return stream;
-}
-
 int main() {
-    cout << "hello world!" << endl;
-    String s1("hello");
-    cout << s1 << endl;
-    return 0;
+	cout << "Test run begins!" << endl;
+
+	String str1; //default constructor
+	String str2 = "hello"; //parameterised constructor
+	String str3 = str1; //copy constructor
+	str3 = str2; //assignment operator
+	int len = str2.length();
+	//cout << str2; //overload <<
+	//cin >> str1; //overload >>
+	cout << str2.length() << endl;
+	cout << "Test run ends!" << endl;
+	return 0;
 }
+
+String::String(const char* ch) {
+
+	if (ch == nullptr) {
+		String();
+		return;
+		}
+
+	len = 0;
+	while (ch[len] != '\0')
+		len++;
+	str = new char[len + 1];
+	strcpy(ch);
+}
+
+String::String(const String& obj) {
+	len = obj.len;
+	str = new char[len + 1];
+	strcpy(obj.str);
+}
+
+String& String::operator=(const String& obj) {
+	if (this != &obj) {
+		delete[] this->str;
+		len = obj.len;
+		str = new char[len + 1];
+		strcpy(obj.str);
+	}
+	//cout << "Assignment operator" << endl;
+	return *this;
+}
+
+void String::strcpy(const char* ch) {
+	if (ch == nullptr)
+		return;
+	char* temp = str;
+	while ((*temp++ == *ch++) != '\0') {}
+	*temp = '\0';
+}
+
+//Notes:
+//1. Operator overloading can also be done in two ways: 
+//		i.	void operator =(const String& obj);
+//		ii.	String& operator =(const String& obj);
+//		In both cases, the assignment operator works as expected.
+//		The only difference being, in case(i), (str1 = str2 = str3)
+//			will give an error as there is no return type defined
